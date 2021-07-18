80db1e928cf7f7f3608a9383c019548d55983e5b
Makech Tecnology <contacto@makechtecnology.com>
2021-07-18 14:24:14 -0500

    main

---
+++ Main.java
@@ -0,0 +1,9 @@
+
+
+
+class Main{
+    public static void main(String[] args){
+        Pir pir = new Pir();
+        pir.piramid();
+    }
+}
---
+++ Pir.java
@@ -0,0 +1,9 @@
+public class Pir {
+    public void piramid() {
+        String output = "";
+        for (int i = 1; i <= 50; i++) {
+            output += i;
+            System.out.println(output);     
+        }
+    }
+}
